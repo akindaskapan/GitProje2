5dfc9b825bb313b1b0d853919cd3fc7b3f0e8ccc
akindaskapan <akin.daskapan@hotmail.com>
2023-09-01 20:52:39 +0300

    Ogrenci classında ikinci bölüm yapıldı.

--- src/Ogrenci.java
+++ src/Ogrenci.java
@@ -1,5 +1,11 @@
 public class Ogrenci {
     public static void main(String[] args) {
-        System.out.println("Merhaba dünya");
+
+        System.out.println("merhaba dünya 2");
+        System.out.println("merhaba dünya 3");
+
+
+
+
     }
 }
